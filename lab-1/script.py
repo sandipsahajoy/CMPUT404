5fb0f5f871db89a66493e2e159ecd989c4aa6013
Sandip Saha Joy <sandipsahajoy@gmail.com>
2022-01-14 06:54:25 -0700

    Update script.py

--- lab-1/script.py
+++ lab-1/script.py
@@ -5,6 +5,7 @@ r = requests.get("http://www.google.com")
 print(r.url)
 print(r.status_code)
 print(r.headers)
+print()
 
 R = requests.get("https://raw.githubusercontent.com/sandipsahajoy/CMPUT404/main/lab-1/script.py")
 print(R.text)
